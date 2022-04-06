e57d8ef72c3445cccb47f81c24217fee41687b52
giorgio <giorgiolink94@hotmail.it>
2022-04-06 10:18:08 +0100

    Update
    cors installed

--- src/server.js
+++ src/server.js
@@ -2,14 +2,19 @@
 import express from "express" // <-- NEW IMPORT SYNTAX (remember to add "type": "module" to package.json to use it!)
 import listEndpoints from 'express-list-endpoints'
 import authorsRouter from "./authors/index.js"
+import cors from "cors"
 
 const server = express()
 const port = 3001
 
 
+
+//middlewares
+
+server.use(cors())
 server.use(express.json())
 
-
+//endopoints
 
 server.use("/authors", authorsRouter)
 
